4d328fc46a65eb0d42a6ecae9f5456c6dad54003
Wais <wais.almakaleh@gmail.com>
2020-05-27 14:37:36 -0400

    added notes

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -13,21 +13,25 @@ module.exports = {
 
 }
 
-
+// finds the schemes database
 function find() {
  return db('schemes')
 }
 
+// finds select information by it's id from the scheme table
 function findById(id){
  return db('schemes')
  .where({id})
  .first();
 }
 
+// finds certain foreign keys by it's id
 function findSteps(id){
   return db( 'schemes' ).join( 'steps', 'schemes.id', 'steps.scheme_id' ).select( 'steps.scheme_id', 'step_number', 'instructions').where('schemes.id', id)
 }
-// ( 'schemes' ).join( 'steps', 'schemes.id', '=', 'steps.scheme_id' ).where( 'scheme_id', id )
+
+
+// adds a new ingredients 
 function add(insert){
  return db('schemes')
  .insert(insert)
@@ -35,14 +39,15 @@ function add(insert){
   return findById(ids[0])
  })
 }
-function addStep(insert, id){
- return db('steps')
-   .insert( {scheme_id: id}, insert)
- .then(ids => {
-  return findById(ids[0])
- })
-}
 
+// adds steps to the ingredients 
+// function addStep(insert, id){
+//  return db('steps')
+//    .insert( {scheme_id: id}, insert)
+//  .then(ids => {
+//   return findById(ids[0])
+//  })
+// }
 function addStep(step, id){
  return db('steps')
    .insert({scheme_id: Number(id), step_number: step.step_number, instructions: step.instructions})
@@ -50,6 +55,8 @@ function addStep(step, id){
  
 }
 
+
+// removes ingredients
 function remove(id) {
  return db('schemes')
  .where('id', id)
